4688dfffa89af48b982b166210308a071508e1f0
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-04-16 22:02:08 -0400

    9 - Create a custom user model

--- src/ecommerce/settings.py
+++ src/ecommerce/settings.py
@@ -130,6 +130,7 @@ STATIC_URL = 'static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+AUTH_USER_MODEL = 'base.User'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
