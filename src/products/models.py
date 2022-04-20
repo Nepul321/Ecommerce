f90c9eb83e34faa79586f4d9426a5005dee9c194
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-04-20 16:12:57 -0400

    11 - Create a products model

--- src/products/models.py
+++ src/products/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    digital = models.BooleanField(default=False)
+    slug = models.SlugField()
+    main_image = models.ImageField(upload_to="posted_images/%Y/%m/%d/%H/%M")
+    image1 = models.ImageField(upload_to="posted_images/%Y/%m/%d/%H/%M", blank=True, null=True)
+    image2 = models.ImageField(upload_to="posted_images/%Y/%m/%d/%H/%M", blank=True, null=True)
+    image3 = models.ImageField(upload_to="posted_images/%Y/%m/%d/%H/%M", blank=True, null=True)
