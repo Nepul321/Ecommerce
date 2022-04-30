98ce48c18bb55c33258bed30d6762be005da5456
Nepul Kahandawa <nepulkahandawa7f@gmail.com>
2022-04-29 23:49:53 -0400

    27 - Create a shipping address model

--- src/shipping/models.py
+++ src/shipping/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from base.models import User
+from orders.models import (
+    Order
+)
 
-# Create your models here.
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    country = models.CharField(max_length=255)
+    order = models.OneToOneField(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.CharField(max_length=255, null=True)
+    city = models.CharField(max_length=255, null=True)
+    state = models.CharField(max_length=255, null=True)
+    zipcode = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return str(self.address)
